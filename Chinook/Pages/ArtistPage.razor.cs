35c712395fa5b7088033fd0c170d4eeb72f3f4b5
Yashodha Athureliyage <yashodha@marsello.com>
2023-02-04 05:20:27 +1300

    Add code behind for ArtistPage

---
+++ Chinook/Pages/ArtistPage.razor.cs
@@ -0,0 +1,83 @@
+﻿using Chinook.ClientModels;
+using Chinook.Models;
+using Chinook.Shared.Components;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace Chinook.Pages
+{
+    public class ArtistPageBase : ComponentBase
+    {
+        [Parameter] public long ArtistId { get; set; }
+        [CascadingParameter] private Task<AuthenticationState> authenticationState { get; set; }
+        [Inject] IDbContextFactory<ChinookContext> DbFactory { get; set; }
+        protected Modal PlaylistDialog { get; set; }
+
+        protected Artist Artist;
+        protected List<PlaylistTrack> Tracks;
+        protected DbContext DbContext;
+        protected PlaylistTrack SelectedTrack;
+        protected string InfoMessage;
+        protected string CurrentUserId;
+
+        protected override async Task OnInitializedAsync()
+        {
+            await InvokeAsync(StateHasChanged);
+            CurrentUserId = await GetUserId();
+            var DbContext = await DbFactory.CreateDbContextAsync();
+
+            Artist = DbContext.Artists.SingleOrDefault(a => a.ArtistId == ArtistId);
+
+            Tracks = DbContext.Tracks.Where(a => a.Album.ArtistId == ArtistId)
+                .Include(a => a.Album)
+                .Select(t => new PlaylistTrack()
+                {
+                    AlbumTitle = (t.Album == null ? "-" : t.Album.Title),
+                    TrackId = t.TrackId,
+                    TrackName = t.Name,
+                    IsFavorite = t.Playlists.Where(p => p.UserPlaylists.Any(up => up.UserId == CurrentUserId && up.Playlist.Name == "Favorites")).Any()
+                })
+                .ToList();
+        }
+
+        protected async Task<string> GetUserId()
+        {
+            var user = (await authenticationState).User;
+            var userId = user.FindFirst(u => u.Type.Contains(ClaimTypes.NameIdentifier))?.Value;
+            return userId;
+        }
+
+        protected void FavoriteTrack(long trackId)
+        {
+            var track = Tracks.FirstOrDefault(t => t.TrackId == trackId);
+            InfoMessage = $"Track {track.ArtistName} - {track.AlbumTitle} - {track.TrackName} added to playlist Favorites.";
+        }
+
+        protected void UnfavoriteTrack(long trackId)
+        {
+            var track = Tracks.FirstOrDefault(t => t.TrackId == trackId);
+            InfoMessage = $"Track {track.ArtistName} - {track.AlbumTitle} - {track.TrackName} removed from playlist Favorites.";
+        }
+
+        protected void OpenPlaylistDialog(long trackId)
+        {
+            CloseInfoMessage();
+            SelectedTrack = Tracks.FirstOrDefault(t => t.TrackId == trackId);
+            PlaylistDialog.Open();
+        }
+
+        protected void AddTrackToPlaylist()
+        {
+            CloseInfoMessage();
+            InfoMessage = $"Track {Artist.Name} - {SelectedTrack.AlbumTitle} - {SelectedTrack.TrackName} added to playlist {{playlist name}}.";
+            PlaylistDialog.Close();
+        }
+
+        protected void CloseInfoMessage()
+        {
+            InfoMessage = "";
+        }
+    }
+}
