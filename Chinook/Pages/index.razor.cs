bef7f77a12acdad513b7ef001facd48f2fa226d4
Yashodha Athureliyage <yashodha@marsello.com>
2023-02-04 05:07:04 +1300

    Add code behind for index.razor

---
+++ Chinook/Pages/index.razor.cs
@@ -0,0 +1,32 @@
+﻿using Chinook.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chinook.Pages
+{
+    public class IndexBase : ComponentBase
+    {
+        protected List<Artist> Artists;
+        [Inject] IDbContextFactory<ChinookContext> DbFactory { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await InvokeAsync(StateHasChanged);
+            Artists = await GetArtists();
+        }
+
+        public async Task<List<Artist>> GetArtists()
+        {
+            var dbContext = await DbFactory.CreateDbContextAsync();
+            var users = dbContext.Users.Include(a => a.UserPlaylists).ToList();
+
+            return dbContext.Artists.ToList();
+        }
+
+        public async Task<List<Album>> GetAlbumsForArtist(int artistId)
+        {
+            var dbContext = await DbFactory.CreateDbContextAsync();
+            return dbContext.Albums.Where(a => a.ArtistId == artistId).ToList();
+        }
+    }
+}
